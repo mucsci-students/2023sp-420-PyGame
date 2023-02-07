99ec97de403779dc48c6c66c71215f3cc1e3e43a
Brendan LeFevre <bjlefevre01@gmail.com>
2023-02-07 16:10:03 -0500

    Updated Player_Rank- more efficient
    Only calculates the difference once now.

--- Player_Rank.py
+++ Player_Rank.py
@@ -3,27 +3,29 @@ class rank:
     # return_Rank takes in current_Points, and max_Points as parameters, then returns the correspending integer
     # to the rank that the player is at.
     # Must be between 0.00 and 1.00
-    def return_Rank(current_Points, max_Points):
+    def return_Rank(currentPoints, maxPoints):
+
+        difference = currentPoints/maxPoints
         #precondition check for between 0.00 and 1.00
-        if current_Points / max_Points <0 or current_Points / max_Points >1:
+        if difference <0 or difference >1:
             return 0 #out of bounds
 
 
-        if current_Points / max_Points >=0 and current_Points / max_Points <=.03:
+        if difference >= 0 and difference < .03:
             return 1 #Beginner
-        elif current_Points / max_Points >=.03 and current_Points / max_Points <.07:
+        elif difference >= .03 and difference < .07:
             return 2 #Novice
-        elif current_Points / max_Points >=.07 and current_Points / max_Points <.12:
+        elif difference >= .07 and difference < .12:
             return 3 #Okay
-        elif current_Points / max_Points >=.12 and current_Points / max_Points <.23:
+        elif difference >= .12 and difference < .23:
             return 4 #Good
-        elif current_Points / max_Points >=.23 and current_Points / max_Points <.35:
+        elif difference >= .23 and difference < .35:
             return 5 #Solid
-        elif current_Points / max_Points >=.35 and current_Points / max_Points <.56:
+        elif difference >= .35 and difference < .56:
             return 6 #Nice
-        elif current_Points / max_Points >=.56 and current_Points / max_Points <.72:
+        elif difference >= .56 and difference < .72:
             return 7 #Great
-        elif current_Points / max_Points >=.72 and current_Points / max_Points <1:
+        elif difference >= .72 and difference < 1:
             return 8 #Amazing
-        elif current_Points / max_Points == 1:
-            return 9 #Genius
+            
+        return 9 #Genius
