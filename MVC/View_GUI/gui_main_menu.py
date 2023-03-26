3edf5a3fc4da4b181386293d41afc13992aff600
KBucher21 <70379821+WhitePolaris@users.noreply.github.com>
2023-03-26 17:12:04 -0400

    Update gui_main_menu.py

--- MVC/View_GUI/gui_main_menu.py
+++ MVC/View_GUI/gui_main_menu.py
@@ -14,7 +14,7 @@ def start_gui():
     pygame.display.set_caption('Main Menu PYGAME')
 
     # Define some colors
-    WHITE = (255, 0, 247)
+    WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
     GRAY = (128, 128, 128)
 
@@ -58,23 +58,23 @@ def start_gui():
         help_box = pygame.draw.polygon(DISPLAY_SURFACE, GRAY, [tuple(map(sum, zip(point, (0, 2 * BOX_HEIGHT + 2 * BOX_SPACING)) )) for point in hexagon_points])
         exit_box = pygame.draw.polygon(DISPLAY_SURFACE, GRAY, [tuple(map(sum, zip(point, (0, 3 * BOX_HEIGHT + 3 * BOX_SPACING)) )) for point in hexagon_points])
 
-        title_text_surface = FONT.render(HEADER_TEXT, True, WHITE)
+        title_text_surface = FONT.render(HEADER_TEXT, True, BLACK)
         title_text_rect = title_text_surface.get_rect(center=((WINDOW_WIDTH/2), BOX_Y_START/2))
         DISPLAY_SURFACE.blit(title_text_surface, title_text_rect)
 
-        new_game_text_surface = FONT.render(NEW_GAME_TEXT, True, WHITE)
+        new_game_text_surface = FONT.render(NEW_GAME_TEXT, True, BLACK)
         new_game_text_rect = new_game_text_surface.get_rect(center=new_game_box.center)
         DISPLAY_SURFACE.blit(new_game_text_surface, new_game_text_rect)
 
-        load_game_text_surface = FONT.render(LOAD_GAME_TEXT, True, WHITE)
+        load_game_text_surface = FONT.render(LOAD_GAME_TEXT, True, BLACK)
         load_game_text_rect = load_game_text_surface.get_rect(center=load_game_box.center)
         DISPLAY_SURFACE.blit(load_game_text_surface, load_game_text_rect)
 
-        help_text_surface = FONT.render(HELP_TEXT, True, WHITE)
+        help_text_surface = FONT.render(HELP_TEXT, True, BLACK)
         help_text_rect = help_text_surface.get_rect(center=help_box.center)
         DISPLAY_SURFACE.blit(help_text_surface, help_text_rect)
 
-        exit_text_surface = FONT.render(EXIT_TEXT, True, WHITE)
+        exit_text_surface = FONT.render(EXIT_TEXT, True, BLACK)
         exit_text_rect = exit_text_surface.get_rect(center=exit_box.center)
         DISPLAY_SURFACE.blit(exit_text_surface, exit_text_rect)
 
