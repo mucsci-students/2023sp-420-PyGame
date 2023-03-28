7a3bcfc416386ced456f5765a2e07db3c97d8933
KBucher21 <70379821+WhitePolaris@users.noreply.github.com>
2023-03-27 20:26:10 -0400

    Update model_hints_module.py

--- MVC/Model/model_hints_module.py
+++ MVC/Model/model_hints_module.py
@@ -1,20 +1,13 @@
-
-#TODO imports
-
-#TODO Abstract the big mess.
-#TODO figure out top and side placement for word length  ???? (maybe just for output)   and side for letters
-
-
-
 """
-    generateHints Takes Three Parameters
+    generateHints 
+    Takes Three Parameters
         Words: List of words from puzzle
         Letters: Array of letters from the model
         Points: Total points in the puzzle
-
-    Returns an array of letters and word lengths  
-    # 1d array of [letters, word_count, points, pangram_count], 2d array of letter counts  with letters and word lengths, 
-    and dictionary Of First Two Letters with the letters as the key and count as the pair
+    Returns
+         1d array of [letters, word_count, points, pangram_count], a list[list[str]] letter counts with 
+         letters and word lengths in the rows and columns, 
+         and dictionary Of First Two Letters with the letters as the key and count as the pair
 """
 def generateHints(words, letters, points): 
     word_count = 0
@@ -24,26 +17,15 @@ def generateHints(words, letters, points):
     
     if(words == [] or len(letters) < 7):
         return 1
-    # will contain the total length, maxLength - 3 will be the total length of the Y axis(includes 4 to max and sum) counting from 0
-    maxLength = 7 #min
-    #[1]
-    #twoLetters_counts = []
-           
-    #words.sort(key=len)
     #gets max length of all words
-    maxLength = len(max(words, key=len))  
-    #maxLength = len(words[-1]) #assumes list is sorted by size
-    #if(maxLength < 7):
-    #    return 1#list not sorted
-    #currently a touple,, Would be treated as a 2d array 
-    #letterCountArray2d = [[0 for x in range(maxLength - 3)] for y in range(8)]  #no words less than leanth of 4    #changed to reflect non inclusive aspect of range to - 3 adn 8   
-    # 
-    # Initialize array
+    maxLength = len(max(words, key=len))   
+    if(maxLength < 7):
+        return 1
+    # Initialize letterCountArray2d 
+    # (maxLength - 1) for maxLength -3 for lengths + total + column label
+    # 9 is for 7 letters + total + row label
+    letterCountArray2d = [[str(0)]*(maxLength - 1)]*9 
     
-    letterCountArray2d = [[str(0)]*(maxLength - 1)]*9 #[0maxLength - 2] #Vectorize  #changed to -3 from -4 
-    
-    
-    #letterCountArray2d[0][0] = str(0) #redundant
     letter_index = 1
     for i in letters:
         letterCountArray2d[letter_index][0] = str(i)
@@ -76,25 +58,16 @@ def generateHints(words, letters, points):
         #adds entry to 2 letter dict 
         if twoLetters not in dictOfFirstTwoLetters: 
             dictOfFirstTwoLetters[twoLetters] = 0
-            #twoLetters_counts.append(0)
         dictOfFirstTwoLetters[twoLetters] += 1
-        #twoLetters_counts[dictOfFirstTwoLetters.index(twoLetters)] += 1        
-        
-        #print(f"{word} first letter {i}, length  {l}")
-        letterCountArray2d[letters.index(first_letter) + 1][current_word_length - 3] = int(letterCountArray2d[letters.index(first_letter) + 1][current_word_length - 3]) + 1 #Check syntax here 
-#    total_score = some function
-    '''
-    #output = letters  #bold  required letter
-    # output += f"words: {word_count} points:  pangrams: {pangram_count}\nLetter Matrix:\n"
-    '''
+        letterCountArray2d[letters.index(first_letter) + 1][current_word_length - 3] = \
+            int(letterCountArray2d[letters.index(first_letter) + 1][current_word_length - 3]) + 1 #Check syntax here 
+
 
 #LETTER MATRIX
     # Totals Rows
     for letter in letters:
-        #row_str = letter + "\t"
         row_total = 0
         for i in range(1, maxLength - 2):#uninclusive end
-            #count = letterCountArray2d[letters.index(letter)][i] 
             row_total += int(letterCountArray2d[letters.index(letter) + 1][i])
         letterCountArray2d [letters.index(letter) + 1][maxLength - 2] = str(row_total)
     # Totals Columns
@@ -105,36 +78,12 @@ def generateHints(words, letters, points):
         letterCountArray2d [8][j] = str(count)
 #ASSEMBLES 2 LETTER COUNTS
     dictOfFirstTwoLetters = sorted(dictOfFirstTwoLetters)
-    '''
-    #str_two_letter_list = f"{dictOfFirstTwoLetters[0]}: {twoLetters_counts[0]} " 
-    #for i in range(1, len(dictOfFirstTwoLetters)): 
-       
-        #compares first letter in 2 letters to previous print, if different, new line
-    #    if((dictOfFirstTwoLetters[i])[0] != (dictOfFirstTwoLetters[i - 1])[0]):
-    #        str_two_letter_list += '\n'
-    #    str_two_letter_list += f"{dictOfFirstTwoLetters[i]}: {twoLetters_counts[i]} " 
-    #output += str_two_letter_list
-    '''
-    #print(output)    
-    #return [letters, {"Word Count: " : word_count}, {"Points: " : points}, {"Number of pangrams: " : pangram_count}], letterCountArray2d, dictOfFirstTwoLetters#, twoLetters_counts
     
-    
-    # returns array of letters word count points and pangram, then a list[list[str]] of the letters and count array, then a dictionary of the two letters words start with
+    letters = str(letters)
+    word_count = str(word_count)
+    points = str(points)
+    pangram_count = str(pangram_count)
+    # returns array of letters word count points and pangram, then a list[list[str]] of the letters and count array, 
+    # then a dictionary of the two letters words start with
     return [letters, word_count, points, pangram_count], letterCountArray2d, dictOfFirstTwoLetters#, twoLetters_counts
-'''
-def generalStats():
-    if(word_count == 0):
-        return 1
-    return [letters, word_count, points, pangram_count]
-def dictOfFirstTwoLetters():
-    if (dictOfFirstTwoLetters == []):
-        return 1
-    return dictOfFirstTwoLetters
-def LetterCountArray2d():
-    if(letterCountArray2d == []):
-        return 1
-    return letterCountArray2d
 
-'''
-
-#generateHints(["whiskey", "whisk"], "whiskey", 2)
