72c545e99225ff51703a36d0153191f806f4c7ca
bennase <121914115+bennase@users.noreply.github.com>
2023-03-27 22:03:01 -0400

    completely redid the inputting
    - removed keyboard module (stupid module idk why I thought it was the only good idea)
    - OS check for inputting text
    - function to check if space key was pressed
    - lots of refactoring and commenting

--- MVC/Controller/CLI_controller.py
+++ MVC/Controller/CLI_controller.py
@@ -3,7 +3,6 @@
 import sys
 import os
 import time
-import keyboard
 
 from model_PuzzleStats import PuzzleStats
 from model_puzzle import Puzzle
@@ -21,79 +20,110 @@ puzzle_stats = PuzzleStats(-1,"")
 
 # auto completes the command name when the TAB key is pressed
 def tab_completion(typed_string, command_set):
-
   # checks to see if the current input can be autocompleted
   if len(typed_string) == 0 or not typed_string.startswith('/'):
-      return typed_string, ''
+    return typed_string, ''
   
-  commands = []
-
+  # gets the correct command set, depending on if the user is on the Main Menu or the Active Game screen
   if command_set == 1:
     commands = ['/newgame', '/loadgame', '/startfromkey', '/startsharedgame', '/help', '/exit']
   elif command_set == 2:
     commands = ['/help', '/back', '/share', '/exit', '/shuffle', '/showall', '/savegame', '/refresh']
 
+  # finds commands that match what the user started to type
   matches = [c for c in commands if c.startswith(typed_string)]
 
+  # split the command up, output the split variables
   if len(matches) == 1:
-    old_string = typed_string
-    typed_string = matches[0]
-    inputted_part, removed_part, ending_letters = typed_string.partition(old_string)
+    typed_string, _, ending_letters = matches[0].partition(typed_string)
     return typed_string, ending_letters
-  else:
-    return typed_string, ''
+
+  # if there was more than one command match, return as if no command was found (do nothing)
+  return typed_string, ''
 
 
+# Collects information about what the user types into the window
 def user_input(command_set):
-    # initialize an empty list to store the typed letters
-    typed_letters = []
+  # initialize an empty list to store the typed letters
+  typed_letters = []
 
-    # listens to key presses
-    keyboard.on_press(lambda event: on_key_press(event, typed_letters, command_set))
-    
-    # wait for keyboard events until Enter key is pressed
-    while True:
-        event = keyboard.read_event()
-        if event.name == 'enter':
-            typed_string = ''.join(typed_letters)
-            typed_letters.clear()
-            keyboard.unhook_all()
-            return typed_string.lower()
-            
+  # wait for keyboard events until enter/return key is pressed
+  while True:
+    key = get_os_name()
+    # if the enter/return key is pressed, return the full typed string; else, figure out the key pressed
+    if key == '\r' or key == '\n':
+      return ''.join(typed_letters).lower()
+    elif key is not None:
+      on_key_press(key, typed_letters, command_set)
 
 
-def on_key_press(event, typed_letters, command_num):
-    # if the tab key is pressed
-    if event.name == 'tab':
-        ending_string = ""
-        typed_string = ''.join(typed_letters)
-        typed_string, ending_string = tab_completion(typed_string, command_num)
-        
-        if ending_string != '':
-          typed_letters.append(ending_string)
-
-        print(typed_string, end = '\r')
-
-    # if the enter key is pressed
-    elif event.name == 'enter':
-        typed_string = ''
-        return False
-
-    # if the backspace key is pressed
-    elif event.name == 'backspace':
-        if typed_letters:
-            typed_letters.pop()
-            print("\033[K", end = "")
-    
-    # if a single alpha character or "/" is pressed (like: /, a, b; not like: shift, backspace)
-    elif (event.name.isalpha() and len(event.name) == 1) or event.name == '/':
-        typed_letters.append(event.name)
-
-    # combine the letters into a word, print on screen
+# Figures out information about the key pressed
+def on_key_press(key, typed_letters, command_num):
+  # if the tab key is pressed (tab completion)
+  if key == '\t':
+    typed_string, ending_string = tab_completion(''.join(typed_letters), command_num)
+    if ending_string:
+      typed_letters.extend(ending_string)
     typed_string = ''.join(typed_letters)
-    print(typed_string, end = '\r')
-    return True
 
+  # if the backspace key is pressed (unix = \xf7; win = \x08)
+  elif key == '\x7f' or key == '\x08':
+    typed_letters and typed_letters.pop()
+    print("\033[K", end="")
+    typed_string = ''.join(typed_letters)
+  
+  # if a single alpha character or '/' is pressed (like: /, a, b; not like: shift, backspace)
+  elif key.isalpha() or key == '/':
+    typed_letters.append(key)
+    typed_string = ''.join(typed_letters)
+
+  # if the key pressed is not tab, backspace, single alpha char, or '/'
+  else:
+    return
+
+  # outputs the final string after the last key is pressed to the console
+  print(typed_string, end='\r')
+
+
+# Figures out the OS name to use correct keyboard for inputting text
+def get_os_name():
+  # if the current os is windows
+  if os.name == 'nt':
+    import msvcrt
+    key = msvcrt.getch()
+    # delete and arrow keys return a character, so exclude them as usable characters for os keyboard
+    if key == b'\x00' or key == b'\xe0':
+      # read the next character to discard escape sequence
+      msvcrt.getch()
+      return None
+    return key.decode('utf-8', 'ignore')
+  
+  # if the current os is unix-based
+  else:
+    import tty
+    import termios
+    file_descriptor = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(file_descriptor)
+    try:
+      tty.setraw(sys.stdin.fileno())
+      char = sys.stdin.read(1)
+      # delete and arrow keys return a character, so exclude them as usable characters for os keyboard
+      if char == '\x1b':
+        # read and discard next two characters of escape sequence
+        sys.stdin.read(2)
+        return None
+    finally:
+      termios.tcsetattr(file_descriptor, termios.TCSADRAIN, old_settings)
+    return char
+
+
+# Checks to see if the space key was pressed
+# "Press the space key to continue.."
+def space_out():
+  key = get_os_name()
+  while True:
+    if key == ' ':
+      break
 
 
 # starts the cli main menu
@@ -106,7 +136,6 @@ def main_menu_handler():
     print("Select a command.")
     userInput = user_input(command_set).lower()
     main_response(userInput)
-
 
 
 # handles users input from the main menu
@@ -127,12 +156,7 @@ def main_response(userInput):
     case "/help":
       cls()
       print_help()
-
-      # user must press space to exit help screen
-      while True:
-        event = keyboard.read_event()
-        if event.name == 'space':
-          break
+      space_out()
         
     case "/exit":
       cls()
@@ -142,6 +166,7 @@ def main_response(userInput):
     case _:
       print("Command Not Recognized")
       time.sleep(1)
+
 
 # when user wants to load a saved game
 def load_save_game():
@@ -193,13 +218,6 @@ def saveGamePrompt():
 
 #Loops thru the active game screens
 def activeGameLoop():
-  # count = -1
-  # for i in puzzle.pangram:
-  #   ++count
-  #   if puzzle.pangram[i] == puzzle.required_letter:
-  #     puzzle[0] == puzzle[count]
-
-
   loop = True
   while (loop):
     cls()
@@ -229,12 +247,7 @@ def active_game_commands(userInput):
     case "/help":
       cls()
       print_help()
-
-      # user must press space to exit help screen
-      while True:
-        event = keyboard.read_event()
-        if event.name == 'space':
-          break
+      space_out()
 
       return True
 
@@ -251,12 +264,7 @@ def active_game_commands(userInput):
     
     case "/showall":
       print_all_guesses(puzzle_stats)
-      
-      # user must press space to exit help screen
-      while True:
-        event = keyboard.read_event()
-        if event.name == 'space':
-          break
+      space_out()
       
       return True
     
@@ -272,12 +280,7 @@ def active_game_commands(userInput):
     case "/share":
       cls()
       print_shared_key_output(puzzle.encode_puzzle_key())
-      
-      # user must press space to exit help screen
-      while True:
-        event = keyboard.read_event()
-        if event.name == 'space':
-          break
+      space_out()
 
       return True
 
@@ -374,10 +377,10 @@ def start_shared_game():
     print("Press 'n' to quit to main menu...")
 
     while True:
-      event = keyboard.read_event()
-      if event.name == 'space':
+      key = get_os_name()
+      if key == ' ':
         return start_shared_game()
-      elif event.name == 'n':
+      elif key == 'n':
         return True
 
   puzzle = prep_value
