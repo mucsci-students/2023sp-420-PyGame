f1dad3dd56b5a5ac27ad1bec38f6866239ac1202
WhitePolaris <70379821+WhitePolaris@users.noreply.github.com>
2023-04-19 23:33:27 -0400

    Create test_puzzle_save_load.py

---
+++ PyGame_Project/Test/test_puzzle_save_load.py
@@ -0,0 +1,88 @@
+import os, sys, pytest
+
+if os.name!="nt": 
+    sys.path.append(os.getcwd()+"/PyGame_Project/MVC")
+    sys.path.append(os.getcwd()+"/PyGame_Project/MVC/Controller")
+    sys.path.append(os.getcwd()+"/PyGame_Project/MVC/Model")
+    sys.path.append(os.getcwd()+"/PyGame_Project/MVC/Model/Database")
+else:
+    sys.path.append(os.getcwd()+"\\PyGame_Project\\MVC")
+    sys.path.append(os.getcwd()+"\\PyGame_Project\\MVC\\Controller")
+    sys.path.append(os.getcwd()+"\\PyGame_Project\\MVC\\Model")
+    sys.path.append(os.getcwd()+"\\PyGame_Project\\MVC\\Model\\Database")
+
+
+
+from model_puzzle import *
+from controller_universal import *
+pytest.fixture
+
+def puzzleGen():
+    PuzzleStats().clear()
+    shareable_key = "ygyxjrfq"
+    prep_game_from_share(shareable_key)
+    
+    
+    
+"""
+Tests to load a saved game without encryption
+""""""
+def test_save():
+    PuzzleStats().clear()
+    puzzleGen()
+    somewordList = Puzzle().current_word_list 
+    PuzzleStats().get_save_game("didi", False)
+    assert somewordList == PuzzleStats().wordList
+def test_load():
+    PuzzleStats().clear()
+    puzzleGen()"""
+"""
+Tests to load a saved game with encryption
+""""""
+def test_save_load_with_encrypt():
+    PuzzleStats().clear()
+    puzzleGen()
+    somewordList = Puzzle().current_word_list   
+    PuzzleStats().get_save_game("yidi", True)
+    PuzzleStats().LoadGame("yidi")
+    assert somewordList == PuzzleStats().wordList"""
+
+""" def test_save_file():
+
+    ##Generate a puzzle, relied on puzzle generation with share code and check guesses
+    PuzzleStats().clear()
+    shareable_key = "ygyxjrfq"
+    share_set = {"m", "e", "a", "t", "b", "l", "s"}
+    center_letter = "t"
+    prep_value = prep_game_from_share(shareable_key)
+    generated_share_set = set(PuzzleStats().pangram)
+
+    ## input some test values to check later
+    PuzzleStats().get_check_guess("tale")
+    PuzzleStats().get_check_guess("tales")
+
+    ## Save the puzzle
+    fileName = "test_1"
+    PuzzleStats().get_save_game(fileName)
+
+    ## Check saved valuse are correct
+    saveFile = "Saves/" + fileName + ".json"
+    
+    ## if file doenst exist will throw an exception, not passing the test
+    ## reads the json file as a Dict
+    with open(saveFile, "r") as openfile:
+        saveInfo = json.load(openfile)
+
+
+    check_1 = saveInfo["CurrentPoints"] == 2
+    check_2 = len(saveInfo["GuessedWords"]) == 2
+    check_3 = saveInfo["MaxPoints"] > 0
+    check_4 = saveInfo["WordList"] > 0
+    check_5 = saveInfo["RequiredLetter"] == center_letter
+    check_6 = len(set(saveInfo["PuzzleLetters"]).intersection(share_set)) == 7
+
+    
+    assert check_1 and check_2 and check_3 and check_4 and check_4 and check_5 and check_6
+     """
+
+
