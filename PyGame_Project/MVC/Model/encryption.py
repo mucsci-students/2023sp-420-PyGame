c21aadd006b46bbc8e8fc933cede0f2fe6df8ce8
WhitePolaris <70379821+WhitePolaris@users.noreply.github.com>
2023-04-20 00:22:23 -0400

    not sure why this propted, but will send

--- PyGame_Project/MVC/Model/encryption.py
+++ PyGame_Project/MVC/Model/encryption.py
@@ -1,8 +1,7 @@
 
+import hashlib, os
 from base64 import b64encode, b64decode
-import hashlib
 from Cryptodome.Cipher import AES
-import os
 from Cryptodome.Random import get_random_bytes
 
 ## -------- encrypt / decrypt ----------- ##
