e1818dcdc8e59e44aab41663f84d97f65e7e4d1f
WhitePolaris <70379821+WhitePolaris@users.noreply.github.com>
2023-04-22 20:05:32 -0400

    abusing the git push to test agian

--- PyGame_Project/Test/test_puzzle_save_load.py
+++ PyGame_Project/Test/test_puzzle_save_load.py
@@ -21,10 +21,10 @@ Tests to load a saved game without encryption
 def test_save_load_without_encryption():
     PuzzleStats().clear()
     puzzleGen()
-    somewordList = [bytes("conflux"), bytes("flux")]
+    somewordList = ["conflux", "flux"]
     PuzzleStats().get_save_game("didi", False)
     PuzzleStats().LoadGame("didi")
-    assert somewordList == PuzzleStats().wordList
+    assert somewordList.decode("utf-8") == PuzzleStats().wordList
 
 """
 Tests to load a saved game with encryption
@@ -32,10 +32,10 @@ Tests to load a saved game with encryption
 def test_save_load_with_encrypt():
     PuzzleStats().clear()
     puzzleGen()
-    somewordList = [bytes("conflux"), bytes("flux")]
+    somewordList = ["conflux", "flux"]
     PuzzleStats().get_save_game("yidi", True)
     PuzzleStats().LoadGame("yidi")
-    assert somewordList == PuzzleStats().wordList
+    assert somewordList.decode("utf-8") == PuzzleStats().wordList
 
 
 """ def test_save_file():
