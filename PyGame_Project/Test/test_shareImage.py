655bc96e8b4b2f75fd519ab5dfa85cbd374c1564
EthanWright24 <ethanwr20@gmail.com>
2023-05-05 20:58:31 -0400

    Added tests
    Trying to raise code cov
    
    added imgae gen for test
    
    added remaining function from puzzle stats
    
    Commented out unused function in game DB

---
+++ PyGame_Project/Test/test_shareImage.py
@@ -0,0 +1,38 @@
+import os, sys, pytest
+
+from PyGame_Project.MVC.Model.model_puzzle import *
+from PyGame_Project.MVC.Model.imageGen import *
+from PyGame_Project.MVC.Controller.controller_universal import *
+
+pytest.fixture
+def puzzleGen():
+    PuzzleStats().clear()
+    shareable_key = "csqkhzct"
+    prep_game_from_share(shareable_key)
+
+    
+"""
+Tests image share function, checks to see if a newly creaded PNG is made
+"""
+def test_share_imageGen():
+    puzzleGen()
+
+    ## makes list of everything in the current direcotry 
+    oldDirectory = os.listdir(os.getcwd())
+
+    generateImage()
+    
+    ## makes list of everything in the current direcotry, after the image is created 
+    newDirectory = os.listdir(os.getcwd())
+
+    check = False
+    newFileCount = 0
+    for file in newDirectory:
+        ## finds if there is any new files in the current directory, after the imageGen function is called
+        if not file in oldDirectory:
+            ## Check if the new file is a PNG
+            if ".png" in file.lower():
+                newFileCount = 1
+                check = True
+
+    assert check and (newFileCount == 1)
